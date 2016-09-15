5d217947ea0ef352f8d6d54643d4bf07a2eb5bb7
Cepr0 <cepr0@ukr.net>
2016-09-15 22:59:32 +0300

    implemeted  sql query to rank order by votes

---
+++ WalletHub/sql/rankorder.sql
@@ -0,0 +1,6 @@
+# Query to rank order by votes
+# (rank = 10)
+SELECT name, votes, round(votes / 10) as 'rng'
+FROM test.votes
+GROUP BY name, votes, round(votes / 10)
+order by round(votes / 10) DESC;
