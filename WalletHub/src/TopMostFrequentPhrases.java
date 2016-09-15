efdd6245b48f20e743e617d5fa9e971295984cc0
Cepr0 <cepr0@ukr.net>
2016-09-15 18:11:58 +0300

    getTopMostFrequentPhrases() completed

--- WalletHub/src/TopMostFrequentPhrases.java
+++ WalletHub/src/TopMostFrequentPhrases.java
@@ -2,8 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,18 +17,19 @@ public class TopMostFrequentPhrases {
     }
 
     private static Map<String, Integer> getTopMostFrequentPhrases(String fileName, String splitter, int top) throws IOException {
-        Map<String, Integer> topPhrases = new ConcurrentHashMap<>();
-        String splitVal = "\\s*" + splitter + "\\s*";
+        Map<String, Integer> topPhrases = new LinkedHashMap<>();
+        String splitPattern = "(^\\s*)|(\\s*\\" + splitter + "\\s*)|(\\s*$)";
 
         try (Stream<String> fileLines = Files.lines(Paths.get(fileName))) {
             fileLines.forEach(s ->
-                    Arrays.stream(s.split(splitVal))
+                    Arrays.stream(s.split(splitPattern))
+                            .filter(w -> !w.isEmpty())
                             .forEach(w -> topPhrases.put(w, topPhrases.getOrDefault(w, 0) + 1)));
         }
 
         return topPhrases.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Integer::compareTo))
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder(Integer::compareTo)))
                 .limit(top)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, i) -> s, ConcurrentHashMap::new));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, i) -> s, LinkedHashMap::new));
     }
 }
