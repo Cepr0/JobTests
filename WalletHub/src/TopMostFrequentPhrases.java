42a2a8c955220819da8e0992f567c145f862da20
Cepr0 <cepr0@ukr.net>
2016-09-17 14:20:19 +0300

    getTopMostFrequentPhrases() with MapDB implemented (1st attept)

--- WalletHub/src/TopMostFrequentPhrases.java
+++ WalletHub/src/TopMostFrequentPhrases.java
@@ -1,33 +1,30 @@
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TopMostFrequentPhrases {
     public static void main(String[] args) throws IOException {
         String fileName = "c:/tmp/wallethub/test.txt";
-        Map<String, Integer> topPhrases = getTopMostFrequentPhrases(fileName, "|", 100); // replace to 100000
+        Stream<Map.Entry<String, Integer>> topPhrases = getTopMostFrequentPhrases(fileName, "|", 100); // replace to 100000
 
-        topPhrases.entrySet().forEach(System.out::println);
+        topPhrases.forEach(System.out::println);
     }
 
-    private static Map<String, Integer> getTopMostFrequentPhrases(String fileName, String splitter, int top) throws IOException {
-        Map<String, Integer> topPhrases = new LinkedHashMap<>();
-        String splitPattern = "\\"+splitter; //"(^\\s*)|(\\s*\\" + splitter + "\\s*)|(\\s*$)";
+    private static Stream<Map.Entry<String, Integer>> getTopMostFrequentPhrases(String fileName, String splitter, int top) throws IOException {
+        DB db = DBMaker.tempFileDB().fileMmapEnable().make();
+        Map<String, Integer> topPhrases = db.treeMap("phrases", Serializer.STRING, Serializer.INTEGER).create();
 
-//        try (Stream<String> fileLines = Files.lines(Paths.get(fileName))) {
-//            fileLines.forEach(s ->
-//                    Arrays.stream(s.split(splitPattern))
-//                            .map(String::trim)
-//                            .filter(w -> !w.isEmpty())
-//                            .forEach(w -> topPhrases.put(w, topPhrases.getOrDefault(w, 0) + 1)));
-//        }
-//
+        String splitPattern = String.format("\\%s", splitter);
+
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
             reader.lines().forEach(line ->
                     Arrays.stream(line.split(splitPattern))
@@ -38,7 +35,6 @@ public class TopMostFrequentPhrases {
 
         return topPhrases.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Collections.reverseOrder(Integer::compareTo)))
-                .limit(top)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, i) -> s, LinkedHashMap::new));
+                .limit(top);
     }
 }
