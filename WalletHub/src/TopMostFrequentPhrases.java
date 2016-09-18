825a4ddee03bedaccce4f3d7f2448ffe7bcf0648
Cepr0 <cepr0@ukr.net>
2016-09-18 14:41:55 +0300

    getTopMostFrequentPhrases() with using H2 db implemented

--- WalletHub/src/TopMostFrequentPhrases.java
+++ WalletHub/src/TopMostFrequentPhrases.java
@@ -1,27 +1,35 @@
-import org.mapdb.DB;
-import org.mapdb.DBMaker;
-import org.mapdb.Serializer;
+import com.iciql.Db;
+import com.iciql.Iciql.IQColumn;
+import com.iciql.Iciql.IQIndex;
+import com.iciql.Iciql.IQIndexes;
+import com.iciql.Iciql.IQTable;
+import com.sun.istack.internal.NotNull;
+import org.h2.tools.DeleteDbFiles;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Stream;
+import java.util.List;
 
 public class TopMostFrequentPhrases {
-    public static void main(String[] args) throws IOException {
-        String fileName = "c:/tmp/wallethub/test.txt";
-        Stream<Map.Entry<String, Integer>> topPhrases = getTopMostFrequentPhrases(fileName, "|", 100); // replace to 100000
+    private static Db db;
+    private static final Phrase p = new Phrase();
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        db = Db.open("jdbc:h2:./phrases", "sa", "");
+
+        String fileWithPhrases = "c:/tmp/wallethub/test.txt";
+        List<Phrase> topPhrases = getTopMostFrequentPhrases(fileWithPhrases, "|", 4); // replace to 100000
 
         topPhrases.forEach(System.out::println);
+        db.close();
+        DeleteDbFiles.execute("./", "phrases", true);
     }
 
-    private static Stream<Map.Entry<String, Integer>> getTopMostFrequentPhrases(String fileName, String splitter, int top) throws IOException {
-        DB db = DBMaker.tempFileDB().fileMmapEnable().make();
-        Map<String, Integer> topPhrases = db.treeMap("phrases", Serializer.STRING, Serializer.INTEGER).create();
+    private static List<Phrase> getTopMostFrequentPhrases(String fileName, String splitter, int top) throws IOException {
 
         String splitPattern = String.format("\\%s", splitter);
 
@@ -30,11 +38,56 @@ public class TopMostFrequentPhrases {
                     Arrays.stream(line.split(splitPattern))
                             .map(String::trim)
                             .filter(phrase -> !phrase.isEmpty())
-                            .forEach(phrase -> topPhrases.put(phrase, topPhrases.getOrDefault(phrase, 0) + 1)));
+                            .forEach(TopMostFrequentPhrases::put));
         }
 
-        return topPhrases.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder(Integer::compareTo)))
-                .limit(top);
+        return db.from(p).orderByDesc(p.count).orderBy(p.text).limit(top).select();
+    }
+
+    private static void put(String phrase) {
+        Phrase current = db.from(p).where(p.text).is(phrase).selectFirst();
+        if (current != null)
+            current.incrementCount();
+        else
+            current = new Phrase(phrase);
+        db.merge(current);
+    }
+
+    @IQTable
+    @IQIndexes({@IQIndex({"count"})})
+    public static class Phrase {
+        @IQColumn(primaryKey = true, length = 255, trim = true)
+        private String text = "";
+
+        @IQColumn
+        private Integer count = 0;
+
+        public Phrase() {
+        }
+
+        public Phrase(@NotNull String text) {
+            this.text = text;
+            ++count;
+        }
+
+        void incrementCount() {
+            ++count;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        @Override
+        public String toString() {
+            return "Phrase{" +
+                    "text='" + text + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
     }
 }
